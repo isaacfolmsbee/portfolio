5b86891cf964c269d7422de0302c3e823f2af72b
Isaac Folmsbee <if@sent.com>
2021-01-13 21:18:26 -0500

    Basic site started, nav and headers

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+	<Navbar />
+	<div id="content">
+		<div id="intro">
+			<h1>Isaac Folmsbee</h1>
+			<h2>
+				I’m a Full-Stack web developer who loves using
+				new technologies to create clean and efficient websites!
+			</h2>
+
+			<!-- <div class="projects-arrow">
+				<h2>Projects</h2>
+				<h1>\/</h1>
+			</div> -->
+		</div>
+
+		<!-- <Project v-for="(project, name) in projects"
+			:key="name" /> -->
+	</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		Navbar,
+	},
+	created() {
+		document.title = "Testing";
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+	margin: 0;
+	padding: 0;
+}
+
+:root {
+	font-size: 24px
+}
+
+#content {
+	background: #383C44;
+	width: 100vw;
+	height: 100vh;
+}
+
+#intro {
+	width: 60vw;
+	margin: 0 auto;
+	padding-top: 30vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+
+#intro h1 {
+	color: #D6D6D6;
+	font-size: 4rem;
+	font-family: 'Francois One', sans-serif;
+}
+
+#intro h2 {
+	color: #C0C0C0;
+	font-family: 'Strait', sans-serif;
+	text-align: center;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,75 @@
+<template>
+	<div class="wrapper">
+		<div class="img-container">
+			<a href="https://github.com/isaacfolmsbee" 
+				target="_blank" rel="noopener noreferrer">
+				<img src="github.svg" alt="Github icon">
+			</a>
+		</div>
+		<div class="img-container">
+			<a href="https://www.linkedin.com/in/isaac-folmsbee-a72487201/" 
+				target="_blank" rel="noopener noreferrer">
+				<img src="linkedin.svg" alt="Linkedin icon">
+			</a>
+		</div>
+		<div class="img-container">
+			<a href="#">
+				<img src="resume.svg" alt="resume icon">
+			</a>
+		</div>
+
+		<h2 class="contact">Contact</h2>
+	</div>
+</template>
+<script>
+export default {
+	name: "Navbar"
+}
+</script>
+<style scoped>
+.wrapper {
+	position: fixed;
+	width: 6rem;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background: #131A23;
+}
+
+.img-container {
+	width: 100%;
+	height: 5.5rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+img {
+	width: 3rem;
+	height: 3rem;
+	margin: auto 0;
+	cursor: pointer;
+	transition: height 350ms, width 350ms;
+}
+
+img:hover {
+	width: 3.4rem;
+	height: 3.4rem;
+}
+
+.contact {
+	position: absolute;
+	bottom: 1rem;
+	color: #D6D6D6;
+	font-size: 1.5rem;
+	font-family: 'Strait', sans-serif;
+	cursor: pointer;
+	transition: color 350ms;
+}
+
+.contact:hover {
+	color: #ffffff;
+}
+</style>
